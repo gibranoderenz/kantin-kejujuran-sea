51f31fd98be92ae8da72ab819f3a2e9d6c0f58d2
gibranoderenz123@gmail.com <gibranoderenz123@gmail.com>
2022-07-10 03:14:57 +0700

    Make fourth commit

--- backend/settings.py
+++ backend/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
